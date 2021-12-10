c87966032b4f2cc90270b516e982d0d788a7da42
Delta-1222 <2572844516@qq.com>
2021-12-10 15:12:06 +0800

    作业7

---
+++ codelist11/src/codelist11/DistinctNumbers.java
@@ -0,0 +1,29 @@
+package codelist11;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class DistinctNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list =new ArrayList<>();
+		
+		Scanner input =new Scanner(System.in);
+		System.out.print("Enter integers (input ends eith 0): ");
+		int value;
+		
+		do {
+			value = input.nextInt();
+			
+			if(!list.contains(value) && value !=0)
+				list.add(value);
+		}while(value != 0);
+		
+		for(int i=0;i<list.size();i++) {
+			System.out.print(list.get(i) + " ");
+		}
+	}
+
+}
